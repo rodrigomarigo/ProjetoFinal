a2340672364c34e19f71cd58f2f63b7e31b90364
rodrigomarigo <rodrigomarigo@gmail.com>
2020-06-07 18:06:04 -0300

    Criado o template inicial para funcionários e adicionado o mapping em FuncionarioController

--- projetofinal/src/main/java/com/example/projetofinal/Controller/FuncionarioController.java
+++ projetofinal/src/main/java/com/example/projetofinal/Controller/FuncionarioController.java
@@ -1,5 +1,25 @@
 package com.example.projetofinal.Controller;
 
+import com.example.projetofinal.Entity.Funcionario;
+import com.example.projetofinal.Service.FuncionarioService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class FuncionarioController {
-    
+    @Autowired
+    private FuncionarioService funcionarioService;
+
+    @GetMapping("/funcionarios")
+    public ModelAndView getFuncionarios(){
+        ModelAndView mv = new ModelAndView("templateFuncionarios");
+
+        mv.addObject("funcionario", new Funcionario());
+        mv.addObject("funcionarios", funcionarioService.getFuncionarios());
+
+        return mv;
+    }
 }
