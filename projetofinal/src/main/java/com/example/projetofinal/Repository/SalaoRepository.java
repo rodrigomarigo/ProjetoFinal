83655f678e1da45e8617d0d1e4689b5ea05ceaa0
rodrigomarigo <rodrigomarigo@gmail.com>
2020-06-07 10:33:47 -0300

    Declarados os repositórios de todas as entidades

---
+++ projetofinal/src/main/java/com/example/projetofinal/Repository/SalaoRepository.java
@@ -0,0 +1,9 @@
+package com.example.projetofinal.Repository;
+
+import com.example.projetofinal.Entity.Salao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SalaoRepository extends JpaRepository <Salao, Integer>{
+    
+}
