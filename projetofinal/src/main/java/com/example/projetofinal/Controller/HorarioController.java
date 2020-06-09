013799ad024df474ed7727fdd97d19a987a22fe8
rodrigomarigo <rodrigomarigo@gmail.com>
2020-06-08 21:16:25 -0300

    Criados métodos no controle para salvar Procedimento, Funcionario e Cliente em um Horario

--- projetofinal/src/main/java/com/example/projetofinal/Controller/HorarioController.java
+++ projetofinal/src/main/java/com/example/projetofinal/Controller/HorarioController.java
@@ -1,7 +1,13 @@
 package com.example.projetofinal.Controller;
 
+import com.example.projetofinal.Entity.Cliente;
+import com.example.projetofinal.Entity.Funcionario;
 import com.example.projetofinal.Entity.Horario;
+import com.example.projetofinal.Entity.Procedimento;
+import com.example.projetofinal.Service.ClienteService;
+import com.example.projetofinal.Service.FuncionarioService;
 import com.example.projetofinal.Service.HorarioService;
+import com.example.projetofinal.Service.ProcedimentoService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,12 +15,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HorarioController {
     @Autowired
     private HorarioService horarioService;
+
+    @Autowired
+    private ClienteService clienteService;
+
+    @Autowired
+    private FuncionarioService funcionarioService;
+
+    @Autowired
+    private ProcedimentoService procedimentoService;
 
     @GetMapping("/horarios")
     public ModelAndView getHorarios(){
@@ -48,5 +64,38 @@ public class HorarioController {
 
         return mv;
     }
+
+    @PostMapping("/associarHorarioCliente")
+    public String associarCliente(@ModelAttribute Cliente cliente, @RequestParam Integer idHorario) {
+        Horario horario = horarioService.getHorarioById(idHorario);
+        cliente = clienteService.getClienteById(cliente.getId());
+
+        horario.setCliente(cliente);
+        horarioService.salvar(horario);
+
+        return "redirect:/detalhesHorario/" + idHorario;
+    }
     
+    @PostMapping("/associarHorarioFuncionario")
+    public String associarFuncionario(@ModelAttribute Funcionario funcionario, @RequestParam Integer idHorario) {
+        Horario horario = horarioService.getHorarioById(idHorario);
+        funcionario = funcionarioService.getFuncionarioById(funcionario.getId());
+
+        horario.setFuncionario(funcionario);
+        horarioService.salvar(horario);
+
+        return "redirect:/detalhesHorario/" + idHorario;
+    }
+
+    @PostMapping("/associarHorarioProcedimento")
+    public String associarProcedimento(@ModelAttribute Procedimento procedimento, @RequestParam Integer idHorario) {
+        Horario horario = horarioService.getHorarioById(idHorario);
+        procedimento = procedimentoService.getProcedimentoById(procedimento.getId());
+
+        horario.setProcedimento(procedimento);
+        horarioService.salvar(horario);
+
+        return "redirect:/detalhesHorario/" + idHorario;
+    }
+
 }
